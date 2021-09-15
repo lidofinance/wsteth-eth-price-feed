4b98f4910b4552a8fd8e68a169e3ce8ef62b1918
Dmitri Ivakhnenko <dmit.ivh@gmail.com>
2021-09-15 19:22:41 +0300

    Price feed: improve calculations

--- contracts/WstETH2ETHPriceFeed.sol
+++ contracts/WstETH2ETHPriceFeed.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.6;
+pragma solidity 0.6.6;
 
 interface IWstETH {
     /**
@@ -21,12 +21,27 @@ contract WstETH2ETHPriceFeed is IChainlinkAggregator {
     IWstETH internal immutable _wstETH;
     IChainlinkAggregator internal immutable _stETH2ETHPriceFeed;
 
+    uint256 constant internal multiplier = 10 ** 18;
+
     constructor(
         address _stETH2ETHPriceFeedAddress,
         address _wstETHAddress
     ) public {
         _stETH2ETHPriceFeed = IChainlinkAggregator(_stETH2ETHPriceFeedAddress);
         _wstETH = IWstETH(_wstETHAddress);
+    }
+
+    function mul(uint256 a, uint256 b) internal view returns (uint256) {
+        /**
+         * @notice Multiply with overflow checking
+         */
+        if (a == 0) {
+            return 0;
+        }
+
+        uint256 c = a * b;
+        assert(c / a == b);
+        return c;
     }
 
     function latestAnswer() external view override returns (int256) {
@@ -36,7 +51,11 @@ contract WstETH2ETHPriceFeed is IChainlinkAggregator {
         int256 stETH2ETH = _stETH2ETHPriceFeed.latestAnswer();
 
         if (stETH2ETH > 0) {
-            return int256(uint256(stETH2ETH) / _wstETH.stEthPerToken());
+            int256 wstETH2ETH = int256(mul(
+                uint256(stETH2ETH), multiplier
+            ) / _wstETH.stEthPerToken());
+            assert(wstETH2ETH > 0);
+            return wstETH2ETH;
         } else {
             return stETH2ETH;
         }
